167b64c38155e93fa01eef7273d1cbb29784cbe7
WEI-HAN CHEN <116339924+henrycwh@users.noreply.github.com>
2025-02-07 17:00:55 +0800

    Add files via upload

--- midi_palyer_simple_edition/AI_Midi_Receptor.cs
+++ midi_palyer_simple_edition/AI_Midi_Receptor.cs
@@ -10,37 +10,44 @@ using System.Net.Http;
 using NAudio.Wave;
 using System.Drawing;
 using Microsoft.VisualBasic.ApplicationServices;
+using System.Text.Json;
 
 namespace midi_palyer_simple_edition
 {
     internal class AI_Midi_Receptor : Form1
     {
-        //private string head_path = "C:\\Users\\chenc\\Desktop\\net6.0-windows";
-        private string head_path = "C:\\Users\\User\\CIoT\\Music_Generation\\midi_palyer_simple_edition\\midi_palyer_simple_edition\\bin\\Debug\\net6.0-windows\\midi_data";
+        private string head_path = AppDomain.CurrentDomain.BaseDirectory;
         private string data_folder_name = "midi_data";
         private string data_folder_path;
+        private string exception_file_path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "exception_log.txt");
+        private string download_url;
         private int data_folder_count;
         private int index_p = 0, index_d = 0;
-        //private string exception_file_path = "C:\\Users\\chenc\\Desktop\\net6.0-windows\\exception_file.txt";
-        private string exception_file_path = "C:\\Users\\User\\CIoT\\Music_Generation\\midi_palyer_simple_edition\\exception_file.txt";
         private DateTime currentTime;
-        public AI_Midi_Receptor() {
+
+        public AI_Midi_Receptor()
+        {
+            LoadConfig(); // 載入設定檔
+
             data_folder_path = Path.Combine(head_path, data_folder_name);
             Directory.CreateDirectory(data_folder_path);
+
         }
+
         void download_new_file()
         {
             using (WebClient c = new WebClient())
             {
-                try {
+                try
+                {
                     // 在這裡執行與 WebClient 相關的工作
                     // 不需要手動呼叫 Dispose 方法，它將在 using 區塊結束時自動呼叫
                     string file = (index_d++) + ".wav";
                     Console.WriteLine("Downloading... " + file);
                     if (File.Exists(file)) File.Delete(file);
-                    c.DownloadFile("http://140.116.86.242:28001/music_api/music_generate", Path.Combine(data_folder_path, file));
+                    c.DownloadFile(download_url, data_folder_path);
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     // 使用 StreamWriter 開啟檔案，如果不存在則建立檔案，並設定編碼為 UTF-8
                     using (StreamWriter writer = new StreamWriter(exception_file_path, true, System.Text.Encoding.UTF8))
@@ -49,15 +56,16 @@ namespace midi_palyer_simple_edition
                         // 寫入文字至檔案
                         writer.WriteLine("Time: " + currentTime + "  error: " + e.Message);
                     }
-                
+
                 }
-            } 
+            }
         }
 
         public void Start_Download()
         {
-            new Thread(() =>{
-                while (true){
+            new Thread(() => {
+                while (true)
+                {
                     data_folder_count = Directory.GetFiles(data_folder_path).Length;
                     if (data_folder_count < 30)
                     {
@@ -72,7 +80,7 @@ namespace midi_palyer_simple_edition
         {
             new Thread(() => {
                 string p_file_path = data_folder_path + "\\" + (index_p++) + ".wav";
-                    
+
                 while (true)
                 {
                     if (File.Exists(p_file_path))
@@ -91,11 +99,11 @@ namespace midi_palyer_simple_edition
                             currentTime = DateTime.Now;
                             // 寫入文字至檔案
                             writer.WriteLine("Time: " + currentTime + "  error: " + "File does not exist.");
-                            
+
                         }
                     }
                 }
-                
+
             }).Start();
         }
         private static void PlayWav(string wavFilePath)
@@ -115,7 +123,7 @@ namespace midi_palyer_simple_edition
             }
             catch (Exception e)
             {
-                string exception_file_path = "C:\\Users\\User\\CIoT\\Music_Generation\\midi_palyer_simple_edition\\exception_file.txt";
+                string exception_file_path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "exception_log.txt");
                 // 使用 StreamWriter 開啟檔案，如果不存在則建立檔案，並設定編碼為 UTF-8
                 using (StreamWriter writer = new StreamWriter(exception_file_path, true, System.Text.Encoding.UTF8))
                 {
@@ -125,6 +133,41 @@ namespace midi_palyer_simple_edition
                 }
             }
         }
+
+        private void LoadConfig()
+        {
+            try
+            {
+
+                string configFilePath = Path.Combine(head_path, "config.json");
+
+                if (!File.Exists(configFilePath))
+                {
+                    throw new FileNotFoundException("設定檔未找到: " + configFilePath);
+                }
+
+                string json = File.ReadAllText(configFilePath);
+                var config = JsonSerializer.Deserialize<Config>(json);
+
+                if (config == null) throw new Exception("設定檔內容無法解析");
+
+                download_url = config.download_url;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("載入設定檔時發生錯誤: " + e.Message);
+                throw;
+            }
+        }
+
+        // 定義設定檔的類別 (對應 JSON 結構)
+        public class Config
+        {
+            public string head_path { get; set; }
+            public string exception_file_path { get; set; }
+            public string download_url { get; set; }
+        }
+
     }
 }
 
